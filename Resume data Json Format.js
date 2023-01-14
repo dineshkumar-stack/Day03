bb9a63dd3c2e38c774ab326d1cc27bd6ad037015
Dinesh Kumar <119421483+dineshkumar-stack@users.noreply.github.com>
2023-01-14 00:04:05 -0500

    Add the theory
    Add the theory

---
+++ Resume data Json Format.js
@@ -0,0 +1,58 @@
+let myResume={
+    "basics": {
+      "name": "Dinesh Kumar",
+      "email": "stardinesh4@gamil.com",
+      "phone": 8682935481,
+      "degree": "MBA",
+      "location": {
+        "address": "Anna nagar chennai",
+        "postalCode": 600101,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+        
+          "github":"https://github.com/dineshkumar-stack"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Edgar",
+        "position": "XBRL",
+        "startDate": "2019-06-24",
+        "endDate": "2023-02-28",
+        "summary": "I am the financial analyst in xbrl handle various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Infant Jesus college",
+        "department": "MBA",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 7.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript, xbrl, excel",
+        "level": "beginner",
+     
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Gamer, youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
